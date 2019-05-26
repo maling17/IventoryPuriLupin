af81577acf54d5011c1fa754a0288e3d690caf11
maling17 <kresnaa09@yahoo.com>
2019-05-26 15:25:24 +0700

    membuat layout bagian Entri

---
+++ app/src/main/java/com/example/iventorypurilupin/EntriWhole.java
@@ -0,0 +1,13 @@
+package com.example.iventorypurilupin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EntriWhole extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entri_whole);
+    }
+}
---
+++ app/src/main/java/com/example/iventorypurilupin/SuratJalan.java
@@ -0,0 +1,13 @@
+package com.example.iventorypurilupin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SuratJalan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_surat_jalan);
+    }
+}
